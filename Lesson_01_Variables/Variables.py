6bea685a66c9a4ac24e65dd736da7dc626592109
Mirza Mansoor Ali Baig <mizi14.rozz@gmail.com>
2025-02-26 19:29:45 +0000

    Basics

--- Lesson_01_Variables/Variables.py
+++ Lesson_01_Variables/Variables.py
@@ -1,5 +1,5 @@
-name = 'Test'
-age = 30
+name = 'Test' #String
+age = 30 #Integer
 
 print(name, age)
 
@@ -11,3 +11,18 @@ your_age = 5
 my_Age = 10
 
 print(your_age, my_Age)
+
+t1 = 5 #Integer
+t2 = 5.2 #Float
+t3 = 5.2j #Complex
+t4 = 0xa #Hexadecimal
+t5 = 0o10 #Octal
+
+print(t1, t2, t3, t4, t5)
+
+print(abs(-5)) #Absolute Value
+print(pow(2, 3)) #Power
+print(round(5.5)) #Round
+print(bin(7)) #Binary
+print(hex(15)) #Hexadecimal
+print(oct(8)) #Octal
--- Lesson_02_Data_Types/Data_Types.py
+++ Lesson_02_Data_Types/Data_Types.py
@@ -1,4 +1,26 @@
-name = 'Test'
-age = 30
+# List Data Type
+name_list = ['Jon', '25tc', 'asd']
+print(name_list)
 
-print(name, age)
+name1, name2, name3 = name_list
+print(name1, name2, name3)
+
+# Tuple Data Type
+name_tuple = ('Jon', '25tu', 'abcd')
+print(type(name_tuple))
+
+# Dictionary Data Type
+name_dict = {"name": "Jon", "age": 30, "city": "Toronto"}
+print(type(name_dict))
+
+#Boolean Data Type
+name_boolean = True
+print(type(name_boolean))
+
+#Range Data Type
+name_range = range(10)
+print(type(name_range))
+
+#Bytes Data Type
+name_bytes = b"Mirza"
+print(name_bytes)
